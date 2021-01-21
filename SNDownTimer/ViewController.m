c005e5f4cf352ea419a1bf46f1e9c184b4addd9d
snlo <snloveydus@gmail.com>
2021-01-21 12:45:53 +0800

    update

--- SNDownTimer/ViewController.m
+++ SNDownTimer/ViewController.m
@@ -12,6 +12,10 @@
 
 @interface ViewController ()
 
+@property (nonatomic, copy) NSString * name;
+
+@property (nonatomic, strong) dispatch_source_t timers;
+
 @end
 
 @implementation ViewController
@@ -21,35 +25,49 @@
     // Do any additional setup after loading the view, typically from a nib.
 //	pod trunk push SNDownTimer.podspec --verbose --allow-warnings --use-libraries
 	
-    __block SNDownTimer * timer =
-    [SNDownTimer downTimerWithFrame:86282 interval:60 formatter:@"HH小时mm分钟" startBlock:^(NSString *showTimeString){
-        //第0秒
-        NSLog(@"start - %@",showTimeString);
-    } intervalBlock:^(NSTimeInterval afterSeconds, NSString *showTimeString) {
-        
-        if (afterSeconds == 11) {
-            [timer invalidate];
-            [SNSharedDownTimer invalidate];
-        }
-        NSLog(@"showTimeString - - %@",showTimeString);
-    } completBlock:^{
-        //最后一秒结束
-        NSLog(@"end");
-    }];
+//    __block SNDownTimer * timer =
+//    [SNDownTimer downTimerWithFrame:86282 interval:60 formatter:@"HH小时mm分钟" startBlock:^(NSString *showTimeString){
+//        //第0秒
+//        NSLog(@"start - %@",showTimeString);
+//    } intervalBlock:^(NSTimeInterval afterSeconds, NSString *showTimeString) {
+//
+//        if (afterSeconds == 11) {
+//            [timer invalidate];
+//            [SNSharedDownTimer invalidate];
+//        }
+//        NSLog(@"showTimeString - - %@",showTimeString);
+//    } completBlock:^{
+//        //最后一秒结束
+//        NSLog(@"end");
+//    }];
+//
+//
+//    [SNSharedDownTimer downTimerInterval:2 intervalBlock:^{
+//        NSLog(@"interval");
+//    }];
     
-    
-    [SNSharedDownTimer downTimerInterval:2 intervalBlock:^{
-        NSLog(@"interval");
-    }];
-    
-    
+	
+	NSMutableString * str = [NSMutableString stringWithFormat:@"test"];
+	
+	self.name = str;
+	
+	[str setString:@"hihi"];
+	
+	NSLog(@"%@",self.name);
+	
+	UITextField * textfield ;
+	
+	textfield.attributedText
+	
+	
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+	dispatch_cancel(self.timers);
 }
 
+- (void)dealloc {
+	NSLog(@"%s",__func__);
+}
 
 @end
